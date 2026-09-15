0c7a3aa87f25997525a070bfc2262940c4930d1c
angeljo0801 <angeljo0801@gmail.com>
2026-09-14 22:36:41 -0400

    Añadir app_shell.dart a Memora

---
+++ memora_upgrade/lib/app_shell.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'guide_store.dart';import 'home_page.dart';import 'tutor_page.dart';import 'study_plan_page.dart';
+class AppShell extends StatefulWidget{const AppShell({super.key,required this.store});final GuideStore store;@override State<AppShell> createState()=>_AppShellState();}
+class _AppShellState extends State<AppShell>{int i=0;@override Widget build(BuildContext context)=>Scaffold(body:IndexedStack(index:i,children:[HomePage(store:widget.store),TutorPage(store:widget.store),StudyPlanPage(store:widget.store)]),bottomNavigationBar:NavigationBar(selectedIndex:i,onDestinationSelected:(v)=>setState(()=>i=v),destinations:const [NavigationDestination(icon:Icon(Icons.library_books),label:'Biblioteca'),NavigationDestination(icon:Icon(Icons.psychology),label:'Tutor'),NavigationDestination(icon:Icon(Icons.calendar_month),label:'Plan')])));}
