149d9cc2126bb2328a60751c254f8ed0c0546a3b
angeljo0801 <angeljo0801@gmail.com>
2026-09-16 01:22:14 -0400

    Chain Memora v1.28 exam patch

--- memora_upgrade/v123_background_execution_patch.py
+++ memora_upgrade/v123_background_execution_patch.py
@@ -164,3 +164,5 @@ _v125 = Path(os.environ['GITHUB_WORKSPACE']) / 'memora_upgrade' / 'v125_fast_tut
 exec(compile(_v125.read_text(), str(_v125), 'exec'))
 _v127 = Path(os.environ['GITHUB_WORKSPACE']) / 'memora_upgrade' / 'v127_fast_exam_batches_patch.py'
 exec(compile(_v127.read_text(), str(_v127), 'exec'))
+_v128 = Path(os.environ['GITHUB_WORKSPACE']) / 'memora_upgrade' / 'v128_ai_card_bank_exam_patch.py'
+exec(compile(_v128.read_text(), str(_v128), 'exec'))
